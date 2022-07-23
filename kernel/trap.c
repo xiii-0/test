7bfce4da7937cd535d769bd5941726ddea612fae
fqr <f_13@qq.com>
2022-07-23 22:29:42 +0800

    lab5

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,27 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  }else if(r_scause() == 13 || r_scause() == 15){
+    uint64 va = r_stval();
+    if(p->sz <= va){
+        p->killed = 1;
+    } else if(va < p->trapframe->sp){
+        p->killed = 1;
+    } else{
+      va = PGROUNDDOWN(va);
+      char *mem = kalloc();
+      if(mem == 0){
+        p->killed = 1;
+      } else{
+        memset(mem, 0, PGSIZE);
+        if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+            kfree(mem);
+            p->killed = 1;
+        }
+      }
+    } 
+  }
+  else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
