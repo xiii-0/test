b2c349a401341ca014dcddef641254c4b7ae9360
fqr <f_13@qq.com>
2022-07-28 22:07:31 +0800

    lab9

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -291,6 +291,7 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
+  int depth=0;
 
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
@@ -329,6 +330,29 @@ sys_open(void)
     end_op();
     return -1;
   }
+  
+   while(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+    // 如果访问深度过大，则退出
+    if (depth++ >= 20) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    // 读取对应的 inode
+    if(readi(ip, 0, (uint64)path, 0, MAXPATH) < MAXPATH) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    iunlockput(ip);
+    // 根据文件名称找到对应的 inode
+    if((ip = namei(path)) == 0) {
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+  }
+
 
   if(ip->type == T_DEVICE){
     f->type = FD_DEVICE;
@@ -482,5 +506,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{  
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0)
+    return -1;
+  if(argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  // create a new inode
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+  // write in linked file to data
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) < MAXPATH) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  
+  iunlockput(ip);
+  end_op();
   return 0;
 }
